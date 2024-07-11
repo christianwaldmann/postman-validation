f8f259ddcf26213a7170b3d4e867967499ca4cda
christianwaldmann <christianwaldmann>
2024-07-11 22:27:20 +0200

    Simplify regex by not requiring new line

--- scripts/remove-secrets.sh
+++ scripts/remove-secrets.sh
@@ -7,13 +7,13 @@ cd $HERE
 # Remove secrets from collections
 for FILENAME in $(find ../collections -iname "*.postman_*.json"); do
     echo -n "Removed "
-    perl -0777 -p -i -e '$k+= s/("key": "(?:password|username|Authorization|value)",\n\s*"value": ")(?:\s*[^{]{2,2}.+[^}]{2,2}\s*)(",\n\s*"type": ".*")/\1\2/g; END{print "$k"}' $FILENAME
+    perl -0777 -p -i -e '$k+= s/("key": "(?:password|username|Authorization|value)",\s*"value": ")(?:\s*[^{]{2,2}.+[^}]{2,2}\s*)(",\s*"type": ".*")/\1\2/g; END{print "$k"}' $FILENAME
     echo " secrets from \"$FILENAME\""
 done
 
 # Remove secrets from environments
 for FILENAME in $(find ../environments -iname "*.postman_*.json"); do
     echo -n "Removed "
-    perl -0777 -p -i -e '$k+= s/("key": ".+",\n\s*"value": ").+(",\n\s*"type": "secret")/\1\2/g; END{print "$k"}' $FILENAME
+    perl -0777 -p -i -e '$k+= s/("key": ".+",\s*"value": ").+(",\s*"type": "secret")/\1\2/g; END{print "$k"}' $FILENAME
     echo " secrets from \"$FILENAME\""
 done
