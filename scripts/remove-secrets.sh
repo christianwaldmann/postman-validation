3ea19ee2ff75d84d82774a552ec5074a3f2f1f09
christianwaldmann <christianwaldmann>
2024-07-11 22:24:02 +0200

    Print number of substitutions

--- scripts/remove-secrets.sh
+++ scripts/remove-secrets.sh
@@ -6,12 +6,14 @@ cd $HERE
 
 # Remove secrets from collections
 for FILENAME in $(find ../collections -iname "*.postman_*.json"); do
-    echo "Removing secrets from \"$FILENAME\""
-    perl -0777 -p -i -e 's/("key": "(?:password|username|Authorization|value)",\n\s*"value": ")(?:\s*[^{]{2,2}.+[^}]{2,2}\s*)(",\n\s*"type": ".*")/\1\2/g' $FILENAME
+    echo -n "Removed "
+    perl -0777 -p -i -e '$k+= s/("key": "(?:password|username|Authorization|value)",\n\s*"value": ")(?:\s*[^{]{2,2}.+[^}]{2,2}\s*)(",\n\s*"type": ".*")/\1\2/g; END{print "$k"}' $FILENAME
+    echo " secrets from \"$FILENAME\""
 done
 
 # Remove secrets from environments
 for FILENAME in $(find ../environments -iname "*.postman_*.json"); do
-    echo "Removing secrets from \"$FILENAME\""
-    perl -0777 -p -i -e 's/("key": ".+",\n\s*"value": ").+(",\n\s*"type": "secret")/\1\2/g' $FILENAME
+    echo -n "Removed "
+    perl -0777 -p -i -e '$k+= s/("key": ".+",\n\s*"value": ").+(",\n\s*"type": "secret")/\1\2/g; END{print "$k"}' $FILENAME
+    echo " secrets from \"$FILENAME\""
 done
