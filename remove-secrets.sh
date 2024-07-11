eb855654a7b5cceea07388b2c7e205269c971ad1
christianwaldmann <christianwaldmann>
2024-07-11 22:07:59 +0200

    Remove obsolete scripts

--- remove-secrets.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-for FILENAME in $(find . -iname "*.postman_collection.json"); do
-    echo "Removing secrets from \"$FILENAME\""
-    perl -0777 -p -i -e 's/("key": "(password|username)",\n\s*"value": ")[^"]+(",\n\s*"type": ".*")/\1\3/g' $FILENAME
-done
-
